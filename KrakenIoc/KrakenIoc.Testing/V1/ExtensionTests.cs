14efb93daec01955d6571b682ab4629f2a9594fa
Carlos <carlos@2kpixelstudios.net>
2021-03-04 23:03:41 -0800

    Adjusted ActionExtensions.IsAnonymousMethod(...) to work in .NET Standard 2.0

--- KrakenIoc/KrakenIoc.Testing/V1/ExtensionTests.cs
+++ KrakenIoc/KrakenIoc.Testing/V1/ExtensionTests.cs
@@ -21,6 +21,10 @@ namespace AOFL.KrakenIoc.Testing.V1
             Action callback4 = SomeInternalMethod;
             Action callback5 = SomeStaticInternalMethod;
 
+            //Sanity checks that anonymous actions' names start with '<', while actual methods do not.
+            Assert.IsTrue(callback.Method.Name[0] == '<', "It is assumed in the " + nameof(ActionExtensions.IsAnonymousMethod) + "(...) method that action/method names starting with '<' are anonymous!");
+            Assert.IsFalse(callback4.Method.Name[0] == '<', "It is assumed in the " + nameof(ActionExtensions.IsAnonymousMethod) + "(...) method that actual/methods names that DO NOT start with '<' are actual methods (NOT anonymous)!");
+
             Assert.IsTrue(callback.IsAnonymousMethod());
             Assert.IsTrue(callback2.IsAnonymousMethod());
             Assert.IsTrue(callback3.IsAnonymousMethod());
